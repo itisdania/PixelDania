6a23fa2571e796baddbfb54172ce710abf47bf0e
Dani <67482490+itisdania@users.noreply.github.com>
2021-10-24 17:17:21 +0200

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,7 +1,10 @@
 function showDiv() {
    document.getElementById('summary').style.display = "";
+   document.getElementById("lifestory").style.display = "none";
+   
 }
 
 function show2Div() {
    document.getElementById('lifestory').style.display = "";
+   document.getElementById("summary").style.display = "none";
 }
