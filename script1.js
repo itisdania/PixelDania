4a0487dbef93a4f18fd49c63ccd34ee3f7e837f0
Dani <67482490+itisdania@users.noreply.github.com>
2021-10-24 17:30:33 +0200

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,17 +1,22 @@
 function showDiv() {
   var x = document.getElementById("summary");
-  vax y = document.getElementById("lifestory");
   if (x.style.display === "none") {
     x.style.display = "";
   } else {
     x.style.display = "none";
   }
    
+}
+     
+
+
+
+function show2Div() {
+  vax y = document.getElementById("lifestory");
   if (y.style.display === "none") {
     y.style.display = "";
   } else {
     y.style.display = "none";
   }
-     
-   
+  
 }
