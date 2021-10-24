1d70ed7085a9413638f545061cd0e11f9a3736d7
Dani <67482490+itisdania@users.noreply.github.com>
2021-10-24 17:45:31 +0200

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,7 +1,12 @@
+var x = document.getElementById("summary");
+  var y = document.getElementById("lifestory");
+
 function showDiv() {
-  var x = document.getElementById("summary");
+  
+  
   if (x.style.display === "none") {
     x.style.display = "";
+     y.style.display = "none"
   } else {
     x.style.display = "none";
   }
@@ -10,3 +15,15 @@ function showDiv() {
      
 
 
+function show2Div() {
+  
+  
+  if (y.style.display === "none") {
+    y.style.display = "";
+     x.style.display = "none"
+  } 
+  else {
+    y.style.display = "none";
+  }
+   
+}
